7b5049443940fc49df1f55d99ae8d58a40580733
kaitobq <kaitobq520@gmail.com>
2024-02-05 16:59:14 +0900

    done

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,10 @@
 function changeBackgroundColor(e) {
   // ここに背景色を変える処理を書く。
-
+  let obj = document.getElementById("text");
+  let cbox = document.getElementById("check");
+  if (check.checked) {
+    obj.style.backgroundColor = "red";
+  } else {
+    obj.style.backgroundColor = "transparent";
+  }
 }
---
+++ src/scripts/station15.js
@@ -0,0 +1,4 @@
+function displayList() {
+  let element = document.getElementById("fruits");
+  element.innerHTML = "<ul><li>リンゴ</li><li>イチゴ</li><li>ブドウ</li></ul>";
+}
